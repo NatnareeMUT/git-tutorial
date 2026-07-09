46e59be769844ac1a2b07267ee521c329a81de2c
natnaree <6714110027@mut.ac.th>
2026-07-09 17:09:29 +0700

    dev3

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
     echo "6714110027";
-    <br />
-    echo "Natnaree";
-    echo "doremon";
-    echo "pencil";
-    echo "color";
-    echo "MUT";
-    echo "fahsai";
-    echo "mee";
-    echo "namhom";
-    echo "ice";
+
 ?>
